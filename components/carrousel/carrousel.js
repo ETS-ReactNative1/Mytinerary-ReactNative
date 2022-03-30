1e7da8ee4044cc8113b08a9248f140c87b005d8f
Sebazt <juan.londono29@udea.edu.co>
2022-03-30 17:21:45 -0500

    ultimo cambio

--- components/carrousel/carrousel.js
+++ components/carrousel/carrousel.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Text, Dimensions, StyleSheet, View } from 'react-native';
+import { Text, Dimensions, StyleSheet, ImageBackground, View } from 'react-native';
 import { SwiperFlatList } from 'react-native-swiper-flatlist';
+import cities from './dates';
 
 const Carr = () => (
   <View style={styles.container}>
+    
     <SwiperFlatList autoplay autoplayDelay={2} autoplayLoop index={2} showPagination>
+       
       <View style={[styles.child, { backgroundColor: 'tomato' }]}>
         <Text style={styles.text}>1</Text>
       </View>
-      <View style={[styles.child, { backgroundColor: 'thistle' }]}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={[styles.child, { backgroundColor: 'skyblue' }]}>
-        <Text style={styles.text}>3</Text>
-      </View>
-      <View style={[styles.child, { backgroundColor: 'teal' }]}>
-        <Text style={styles.text}>4</Text>
-      </View>
+       
     </SwiperFlatList>
+    
   </View>
 );
 
