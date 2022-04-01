a1c15bf5b8733d098157f5620ab950c936fb0162
Sebazt <juan.londono29@udea.edu.co>
2022-04-01 07:54:00 -0500

    Arquitectura y crasheo ok

--- App.js
+++ App.js
@@ -3,28 +3,18 @@ import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native'
 /* import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'; */
 import React, { useEffect, useState}  from 'react';
 import Home from './src/Screens/home';
-import * as Font from 'expo-font';
+
+import { useFonts, Caveat_400Regular, Caveat_700Bold } from '@expo-google-fonts/caveat';
+
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
+    let [fontsLoaded, error] = useFonts({
+      Caveat_400Regular,
+      Caveat_700Bold
+    })
 
-  useEffect(()=>{
-    if(!fontLoaded){
-      loadFonts();
-    }
-  });
-
-  const loadFonts = async () =>{
-    await Font.loadAsync({
-      'caveat-Medium': require('./assets/fonts/Caveat-Medium.ttf'),
-    });
-    setFontLoaded(true); 
-  }
-
-   if (!fontLoaded){
-     return(<View/>)
-   }
-
+    
+    
   return (
     <ScrollView style={{ height: '100%', width: '100%', flex: 1, flexDirection: 'column' }}>
       <StatusBar style="auto" backgroundColor='#17F5F9' />
@@ -38,6 +28,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   fontFamily: {
-    fontFamily:'caveat-Medium'
+    fontFamily:'Caveat_400Regular'
   }
 });
